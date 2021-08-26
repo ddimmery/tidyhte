d0db23a632f71f23e1baaa050aae8d6263c48fbd
Drew Dimmery <drew.dimmery@gmail.com>
2021-08-26 16:21:33 +0200

    add pkgdown

--- R/model_cfg.R
+++ R/model_cfg.R
@@ -1,8 +1,31 @@
-
+#' Configuration of SuperLearner Submodel
+#'
+#' @description
+#' `SLLearner_cfg` is a configuration class for a single
+#' sublearner to be included in SuperLearner. By constructing with a named list
+#' of hyperparameters, this configuration allows distinct submodels
+#' for each unique combination of hyperparameters.
 #' @export
 SLLearner_cfg <- R6::R6Class("SLLearner_cfg",
-    list(
+    public = list(
+        #' @field model_name The name of the model as passed to `SuperLearner`
+        #' through the `SL.library` parameter.
         model_name = character(),
+
+        #' @field hyperparameters Named list from hyperparameter name to a vector of
+        #' values that should be swept over.
+
+        #' @description
+        #' Create a new `SLLearner_cfg` object with specified model name and hyperparameters.
+        #' @param model_name The name of the model as passed to `SuperLearner`
+        #' through the `SL.library` parameter.
+        #' @param hp Named list from hyperparameter name to a vector of values that should be
+        #' swept over. Hyperparameters not included in this list are left at their SuperLearner
+        #' default values.
+        #' @return A new `SLLearner_cfg` object.
+        #' @examples
+        #' SLLearner_cfg$new("SL.glm")
+        #' SLLearner_cfg$new("SL.gam", list(deg.gam = c(2, 3)))
         hyperparameters = NULL,
         initialize = function(model_name, hp=NULL) {
             self$model_name <- model_name
@@ -19,24 +42,64 @@ Model_cfg <- R6::R6Class("Model_cfg",
     )
 )
 
+#' Configuration of Known Model
+#'
+#' @description
+#' `Known_cfg` is a configuration class for when a particular model is known
+#' a-priori. The prototypical usage of this class is when heterogeneous
+#' treatment effects are estimated in the context of a randomized control
+#' trial with known propensity scores.
 #' @export
 Known_cfg <- R6::R6Class("Known_cfg",
     inherit = Model_cfg,
-    list(
+    public = list(
+        #' @field covariate_name The name of the column in the dataset
+        #' which corresponds to the known model score.
         covariate_name = character(),
+        #' @field model_class The class of the model, required for all classes
+        #' which inherit from `Model_cfg`.
         model_class = "known",
+
+        #' @description
+        #' Create a new `Known_cfg` object with specified covariate column.
+        #' @param covariate_name The name of the column, a string, in the dataset
+        #' corresponding to the known model score (i.e. the true conditional expectation).
+        #' @return A new `Known_cfg` object.
+        #' @examples
+        #' Known_cfg$new("propensity_score")
         initialize = function(covariate_name) {
             self$covariate_name <- covariate_name
         }
     )
 )
 
+#' Configuration for a Kernel Smoother
+#'
+#' @description
+#' `KernelSmooth_cfg` is a configuration class for non-parametric local-linear
+#' regression to construct a smooth representation of the relationship between
+#' two variables. This is typically used for displaying a surface of the conditional
+#' average treatment effect over a continuous covariate.
 #' @export
 KernelSmooth_cfg <- R6::R6Class("KernelSmooth_cfg",
     inherit = Model_cfg,
-    list(
+    public = list(
+        #' @field covariate_name The name of the column in the dataset
+        #' which corresponds to the known model score.
         model_class = "KernelSmooth",
+        #' @field neval The number of points at which to evaluate the local
+        #' regression. More points will provide a smoother line at the cost
+        #' of somewhat higher computation.
         neval = integer(),
+
+        #' @description
+        #' Create a new `KernelSmooth_cfg` object with specified number of evaluation points.
+        #' @param neval The number of points at which to evaluate the local
+        #' regression. More points will provide a smoother line at the cost
+        #' of somewhat higher computation.
+        #' @return A new `KernelSmooth_cfg` object.
+        #' @examples
+        #' KernelSmooth_cfg$new(neval = 100)
         initialize = function(neval = 100) {
             self$neval <- neval
         }
